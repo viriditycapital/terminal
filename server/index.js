5f75adf83bcf5cc6c459df5a449cb0926ff2acc3
mikinty <youmichaelc@gmail.com>
2021-10-07 01:20:38 -0400

    Ported some graphing code from theta. Doesn't quite work, need to modify it

--- server/index.js
+++ server/index.js
@@ -13,12 +13,22 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/stonks/:ticker", async (req, res) => {
+  let date = new Date();
+  date.setDate(date.getDate() - 30);
   const search = await yahooFinance.search(req.params.ticker);
   const quotes = await yahooFinance.quote(req.params.ticker);
+
+  const historical = await yahooFinance.historical(req.params.ticker, {
+    period1: date,
+    period2: new Date(),
+    interval: '1d'
+  });
+  console.log(date, new Date());
 
   res.send({
     search,
     quotes,
+    historical
   });
 });
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Chart_d3 } from "./plot/plot.js";
+
 /**
  * This is the highest level of the web app.
  */
@@ -29,6 +31,19 @@ class App extends React.Component {
   render() {
     let stock_data = this.state.stock_data;
     let quotes = this.state.stock_data?.quotes;
+    let historical = this.state.stock_data?.historical;
+
+    if (historical != null) {
+      let chart_price = new Chart_d3("chart-price", {
+        top: 20,
+        right: 20,
+        bottom: 50,
+        left: 70,
+      });
+
+      chart_price.plot_line("price", historical, "date", "close", true);
+    }
+
     return (
       <div id="main">
         <div id="header">
@@ -44,11 +59,12 @@ class App extends React.Component {
           </div>
         </div>
         <div id="main-content">
-          Getting quotes for {quotes?.shortName}
-          <div id="chart">
-            <div id="bid">{`$${quotes?.bid} x ${quotes?.bidSize}`}</div>
-            <div id="ask">{`$${quotes?.ask} x ${quotes?.askSize}`}</div>
+          {quotes?.longName}
+          <div id="bid-ask">
+            <div id="bid">{`Bid: $${quotes?.bid} x ${quotes?.bidSize}`}</div>
+            <div id="ask">{`Ask: $${quotes?.ask} x ${quotes?.askSize}`}</div>
           </div>
+          <div id="chart-price"></div>
           <div id="news">
             <div id="news-header">News</div>
             <div id="news-articles">
@@ -58,6 +74,10 @@ class App extends React.Component {
                 </a>
               ))}
             </div>
+          </div>
+          <div id="twitter">
+            Relevant tweets, scrape from fintwit and walter bloomberg
+            (Deltaone), FxHedgers, LiveSquawk
           </div>
         </div>
       </div>
